59c36147c2259db4edb31bb814e2ffb8c108bff2
achirkov <andrey.chirkov@zedconnect.com>
2019-01-09 17:17:08 +0300

    .

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,3 @@
-// Food2fork API key 6f54c1f9c036bf2972510ce04331f5d8 
-// https://www.food2fork.com/api/search
-
-import axios from 'axios';
-
-async function getResults(query) {
-    const key = '6f54c1f9c036bf2972510ce04331f5d8';
-    try {
-        const result = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = result.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
-
-getResults('pizza');
+import Search from './models/Search';
+const search = new Search('pizza');
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        const key = '6f54c1f9c036bf2972510ce04331f5d8';
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
+
