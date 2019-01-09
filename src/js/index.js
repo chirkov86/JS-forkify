d14b49dba939ad8d708c5f644525c3b1ad8039f4
achirkov <andrey.chirkov@zedconnect.com>
2019-01-09 16:53:36 +0300

    added axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
-import string1 from './models/Search';
-import { add, multiply, ID } from './views/searchView';
+// Food2fork API key 6f54c1f9c036bf2972510ce04331f5d8 
+// https://www.food2fork.com/api/search
 
-console.log(`Using imported ${add(ID, 2)} and ${multiply(3, 5)}. ${string1}`);
+import axios from 'axios';
+
+async function getResults(query) {
+    const key = '6f54c1f9c036bf2972510ce04331f5d8';
+    try {
+        const result = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = result.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+
+getResults('pizza');
